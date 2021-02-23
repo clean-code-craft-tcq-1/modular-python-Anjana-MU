4f21ab346194b6cf228183107fbe8afa032ae145
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-02-23 10:19:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@
 from ColorPair_test_data import *
 from ReferenceManual import *
 
-
 if __name__ == '__main__':
   test_functionalities()
   printReferenceManual(createReferenceManual())  
   print('Done :)')
-
