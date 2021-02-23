6c54c334b8a72e41886a0508c6da7d67828d4547
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-02-23 14:20:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from ReferenceManual import *
+from ReferenceManual import createReferenceManual
+from ReferenceManual import printReferenceManual 
 import ColorPair_test_data as td
 
 if __name__ == '__main__':
   td.test_functionalities()
   print('Reference Manual')
-  createReferenceManual = ReferenceManual.createReferenceManual()
   printReferenceManual(createReferenceManual())  
   print('Done :)')
