8c5cc540ead10bab8a7921543d7f8b5a36a72c28
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-02-23 11:58:17 +0530

    Update ColorPair_test_data.py

--- ColorPair_test_data.py
+++ ColorPair_test_data.py
@@ -1,4 +1,4 @@
-from ColorPair_get_data import *
+import ColorPair_get_data as gd 
 
 def test_functionalities():
   test_number_to_pair(4, 'White', 'Brown')
@@ -9,10 +9,10 @@ def test_functionalities():
     
 def test_number_to_pair(pair_number,
                         expected_major_color, expected_minor_color):
-  major_color, minor_color = get_color_from_pair_number(pair_number)
+  major_color, minor_color = gd.get_color_from_pair_number(pair_number)
   assert(major_color == expected_major_color)
   assert(minor_color == expected_minor_color)
 
 def test_pair_to_number(major_color, minor_color, expected_pair_number):
-  pair_number = get_pair_number_from_color(major_color, minor_color)
+  pair_number = gd.get_pair_number_from_color(major_color, minor_color)
   assert(pair_number == expected_pair_number)
