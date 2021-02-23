9bf1d55dfee17140ef0e69544ed0e7c814b9d109
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-02-23 14:04:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from ReferenceManual import createReferenceManual
 from ReferenceManual import printReferenceManual
 import ColorPair_test_data as td
 
+MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
+
 if __name__ == '__main__':
   td.test_functionalities()
   print('Reference Manual')
