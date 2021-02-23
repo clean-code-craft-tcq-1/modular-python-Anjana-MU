0731f16b1c7a27594c190e81f1912b02a2137d20
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-02-23 12:01:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from ColorPair_test_data import test_functionalities
 from ReferenceManual import printReferenceManual
 from ReferenceManual import createReferenceManual
 
 if __name__ == '__main__':
-  test_functionalities()
+  test_number_to_pair(4, 'White', 'Brown')
+  test_number_to_pair(5, 'White', 'Slate')
+  test_pair_to_number('Black', 'Orange', 12)
+  test_pair_to_number('Violet', 'Slate', 25)
+  test_pair_to_number('Red', 'Orange', 7)
   printReferenceManual(createReferenceManual())  
   print('Done :)')
